f52f19dad867b604573551ea7c4c1323bd3b04b2
ailano <52124474+ailano@users.noreply.github.com>
2024-12-18 12:44:57 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+03 Scrivere codice con NodeJS
+
+*/
+
+const numero = 3;
+
+if(numero < 3){
+    console.log("il numero è minore di 3");
+}else if(numero==3){
+    console.log("il numero è uguale a 3");
+} else{
+    console.log("il numero è maggiore di 3");
+}
