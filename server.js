5eb611a050ee0b7f46a4a34b993669b7b7842f1c
ailano <52124474+ailano@users.noreply.github.com>
2024-12-18 13:34:00 +0100

    nuovo file

---
+++ server.js
@@ -0,0 +1,14 @@
+/*
+03 Scrivere codice con NodeJS
+secondo test e terzo e quarto
+*/
+
+const numero = 10;
+
+if(numero < 10){
+    console.log("il numero è minore di 10");
+}else if(numero==3){
+    console.log("il numero è uguale a 10");
+} else{
+    console.log("il numero è maggiore di 10");
+}
